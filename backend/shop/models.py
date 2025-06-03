583a21d4841d27a6d01cbd75f9a5b6655e350aa9
AlxRmn <166999223+AlxRmn@users.noreply.github.com>
2025-06-03 20:42:31 +0300

    Update models.py

--- backend/shop/models.py
+++ backend/shop/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -13,5 +11,4 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-# Добавим связь ManyToMany через User.profile позже, если потребуется кастомизация профиля
 User.add_to_class('products', models.ManyToManyField(Product, blank=True, related_name='buyers'))
